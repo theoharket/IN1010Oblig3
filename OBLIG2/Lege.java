19e8d8ef5146e93ae88e1282ab7cb0a9d48d2687
theoharket <55239037+theoharket@users.noreply.github.com>
2020-03-11 15:58:29 +0100

    Hei!

--- OBLIG2/Lege.java
+++ OBLIG2/Lege.java
@@ -1,9 +1,19 @@
 
 //Definerer en Lege-klasse som tar inn navnet til legen.
-class Lege {
+
+class Lege<T extends Comparable<Lege>> {
+
   String navn;
+  static Liste<String> leger = new SortertLenkeliste<String>();
+  Liste<Resept> utskrevedeResepter = new Lenkeliste<Resept>();
+
   public Lege(String navnetTilLegen){
     navn = navnetTilLegen;
+    _SorterLege(navnetTilLegen);
+  }
+
+  private void _SorterLege(String navn) {
+    leger.leggTil(navn);
   }
   //Returnerer navnet til legen.
   public String hentNavnTilLege() {
@@ -13,4 +23,25 @@ class Lege {
   public String toString() {
     return "Navnet til legen er: " + navn;
   }
+
+  public T hentUtskrevedeResepter(int ID) {
+    return utskrevedeResepter.hent(ID);
+  }
+  /*
+  public T hentleger() {
+    return leger;
+  }*/
+
+
+  /*public HvitResept skrivHvitResept(Legemiddel legemiddel, Pasient pasient, int reit) throws
+UlovligUtskrift;
+
+  public MillitaerResept skrivMillitaerResept(Legemiddel legemiddel, Pasient pasient, int
+  reit) throws UlovligUtskrift;
+
+  public PResept skrivPResept(Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift;
+
+  public BlaaResept skrivBlaaResept(Legemiddel legemiddel, Pasient pasient, int reit) throws
+  UlovligUtskrift;
+*/
 }
