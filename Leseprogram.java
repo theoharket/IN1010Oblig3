c8a60d8b7c4b7d613b1f1abb796f6002347c3e8a
haakon0212 <62065062+haakon0212@users.noreply.github.com>
2020-03-13 12:36:39 +0100

    endring leser

--- Leseprogram.java
+++ Leseprogram.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
 import java.io.File;
 
+
 class Filleser{
     public static void main(String[] args){
-        System.out.println("Filleserprogrammet");
-        System.out.println("Endring 1");
+
+        Pasient Haakon = new Pasient("13123","Haakon");
+        lesFil("Pasienter.txt");
+    }
+
+    public void lesFil(String filnavn){
+
+        Scanner fil = null;
+
+        try{
+          fil = new Scanner(new File(filnavn));
+
+        }catch(Exception e){
+          System.out.println("Feil filnavn!");
+        }
+        while(fil.hasNextLine()){
+            System.out.println();
+            fil.nextline();
+        }
     }
 }
