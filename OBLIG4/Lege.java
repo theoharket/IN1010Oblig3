026cbf4996e03325cba38113d33f163203daf2dc
theoharket <55239037+theoharket@users.noreply.github.com>
2020-03-20 11:20:24 +0100

    Lege-forandringer

--- OBLIG4/Lege.java
+++ OBLIG4/Lege.java
@@ -1,11 +1,14 @@
 
 //Definerer en Lege-klasse som tar inn navnet til legen.
-
+import java.util.*;
 class Lege {
-
+  boolean skrevetUtNarkotisk = false;
+  int antSkrevetNarkotiske =0;
+  int antallNarkotiske = 0;
+  int antallVanedannende = 0;
   String navn;
   int IDLege;
-  static Liste<String> leger = new SortertLenkeliste<String>();
+  static Liste<Lege> leger = new SortertLenkeliste<Lege>();
   Liste<Resept> utskrevedeResepter = new Lenkeliste<Resept>();
 
   public Lege(String navnetTilLegen){
@@ -26,7 +29,7 @@ class Lege {
     return "Navnet til legen er: " + navn;
   }
 
-  /*public void skrivUtAlleLeger() {
+  public void skrivUtAlleLeger() {
     int i =0;
     while(leger.neste!=NULL) {
       navn = leger.hent(i);
@@ -34,20 +37,46 @@ class Lege {
       i++;
     }
   }
-*/
+
+  public boolean finnesLege(String legeNavn) {
+    legeNavn = legeNavn.toLowerCase();
+    for (int i=0; i<leger.stoerrelse();i++) {
+      if (leger.hent(i).toLowerCase()==legeNavn) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public Lege leggTilLegeListe(Lege legen) {
+    legene.leggTil(legen);
+  }
+
   public Resept hentUtskrevedeResepter(int ID) {
     return utskrevedeResepter.hent(ID);
   }
 
   public Hvit skrivHvitResept(Legemiddel legemiddel, Pasient pasient, int reit) throws
 UlovligUtskrift {
-  Hvit nyResept = new Hvit(true, legemiddel, Lege(navn) ,pasient, reit);
+
+  if (legemiddel instanceof Vanedannende) {
+    antallVanedannende++;
+  } else if (legemiddel instanceof Narkotisk) {
+    antallNarkotiske++;
+  }
+  Hvit nyResept = new Hvit(true, legemiddel,pasient, reit);
   utskrevedeResepter.leggTil(nyResept);
   return nyResept;
 }
 
   public MilitaerResept skrivMillitaerResept(Legemiddel legemiddel, Pasient pasient, int
   reit) throws UlovligUtskrift {
+
+      if (legemiddel instanceof Vanedannende) {
+        antallVanedannende++;
+      } else if (legemiddel instanceof Narkotisk) {
+        antallNarkotiske++;
+      }
       MilitaerResept nyResept = new MilitaerResept(true,legemiddel,  pasient, reit);
       pasient.leggTilResept(nyResept);
       utskrevedeResepter.leggTil(nyResept);
@@ -55,6 +84,12 @@ UlovligUtskrift {
   }
 
   public PResept skrivPResept(Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift {
+
+      if (legemiddel instanceof Vanedannende) {
+        antallVanedannende++;
+      } else if (legemiddel instanceof Narkotisk) {
+        antallNarkotiske++;
+      }
       PResept nyResept = new PResept(true, legemiddel,  pasient);
       utskrevedeResepter.leggTil(nyResept);
       return nyResept;
@@ -62,6 +97,11 @@ UlovligUtskrift {
 
   public BlaaResept skrivBlaaResept(Legemiddel legemiddel, Pasient pasient, int reit) throws
   UlovligUtskrift  {
+      if (legemiddel instanceof Vanedannende) {
+        antallVanedannende++;
+      } else if (legemiddel instanceof Narkotisk) {
+        antallNarkotiske++;
+      }
       BlaaResept nyResept = new BlaaResept(true, legemiddel, pasient, reit);
       utskrevedeResepter.leggTil(nyResept);
       return nyResept;
